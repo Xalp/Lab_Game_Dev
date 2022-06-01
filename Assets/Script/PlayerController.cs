12213fdac2a532015bb693c4f37f766ab7ed7825
xalphinions@outlook.com <you@example.com>
2022-06-02 03:24:41 +0800

    Second Lab Commit: Add animation, mushroom and spring

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -13,9 +13,10 @@ public class PlayerController : MonoBehaviour
     private SpriteRenderer marioSprite;
     private bool faceRightState = true;
     public Transform enemyLocation;
-    public Text scoreText;
-    private int score = 0;
-    private bool countScoreState = false;
+    private  Animator marioAnimator;
+    // public Text scoreText;
+    // private int score = 0;
+    // private bool countScoreState = false;
 
     void OnTriggerEnter2D(Collider2D other)
     {
@@ -31,9 +32,18 @@ public class PlayerController : MonoBehaviour
         if (col.gameObject.CompareTag("Ground"))
         {
             onGroundState = true; // back on ground
-            countScoreState = false; // reset score state
-            scoreText.text = "Score: " + score.ToString();
+            marioAnimator.SetBool("OnGround", onGroundState);
+            // countScoreState = false; // reset score state
+            // scoreText.text = "Score: " + score.ToString();
         };
+        if (col.gameObject.CompareTag("Obstacles")&&Mathf.Abs(marioBody.velocity.y)<0.01f)
+        {
+            onGroundState = true; // back on ground
+            marioAnimator.SetBool("OnGround", onGroundState);
+            // countScoreState = false; // reset score state
+            // scoreText.text = "Score: " + score.ToString();
+        };
+
     }
     // Start is called before the first frame update
     void Start()
@@ -42,6 +52,7 @@ public class PlayerController : MonoBehaviour
         Application.targetFrameRate =  30;
         marioBody = GetComponent<Rigidbody2D>();
         marioSprite = GetComponent<SpriteRenderer>();
+        marioAnimator  =  GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -50,22 +61,28 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKeyDown("a") && faceRightState){
             faceRightState = false;
             marioSprite.flipX = true;
+            if (Mathf.Abs(marioBody.velocity.x) >  1.0) 
+	            marioAnimator.SetTrigger("OnSkid");
         }
 
         if (Input.GetKeyDown("d") && !faceRightState){
             faceRightState = true;
             marioSprite.flipX = false;
+            if (Mathf.Abs(marioBody.velocity.x) >  1.0) 
+	            marioAnimator.SetTrigger("OnSkid");
         }
         
-        if (!onGroundState && countScoreState)
-        {
-            if (Mathf.Abs(transform.position.x - enemyLocation.position.x) < 0.5f)
-            {
-                countScoreState = false;
-                score++;
-                Debug.Log(score);
-            }
-        }
+        marioAnimator.SetFloat("xSpeed", Mathf.Abs(marioBody.velocity.x));
+        marioAnimator.SetBool("OnGround", onGroundState);
+        // if (!onGroundState && countScoreState)
+        // {
+        //     if (Mathf.Abs(transform.position.x - enemyLocation.position.x) < 0.5f)
+        //     {
+        //         countScoreState = false;
+        //         score++;
+        //         Debug.Log(score);
+        //     }
+        // }
     }
 
     void FixedUpdate(){
@@ -84,7 +101,7 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKeyDown("space") && onGroundState){
             marioBody.AddForce(Vector2.up * upSpeed, ForceMode2D.Impulse);
             onGroundState = false;
-            countScoreState = true; //check if Gomba is underneath
+            // countScoreState = true; //check if Gomba is underneath
         }
         if (!onGroundState){
             marioBody.AddForce(Vector2.down * 1f, ForceMode2D.Impulse);
---
+++ Assets/Script/Week2MushroomOrangeController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Week2MushroomOrangeController : MonoBehaviour
+{
+    private float originalX;
+    private float maxOffset = 5.0f;
+    private int moveRight = -1;
+    private Vector2 velocity;
+    private Collider2D EnemyCollider;
+    private Rigidbody2D enemyBody;
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("Player")==false && col.gameObject.CompareTag("Ground")==false && col.gameObject.CompareTag("Obstacles")==false)
+        {
+            moveRight = -moveRight;
+        };
+        if (col.gameObject.CompareTag("Player")){
+            moveRight = 0;
+            Destroy(GameObject.Find("MushroomOrange(Clone)"));
+        }
+    }
+    void Start()
+    {
+        enemyBody = GetComponent<Rigidbody2D>();
+        // get the starting position
+        originalX = transform.position.x;
+        ComputeVelocity();
+    }
+    void ComputeVelocity(){
+        velocity = new Vector2((moveRight)*maxOffset, 0);
+    }
+    void MoveGomba(){
+        enemyBody.MovePosition(enemyBody.position + velocity * Time.fixedDeltaTime);
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        // change direction
+        
+        ComputeVelocity();
+        MoveGomba();
+    }
+}
